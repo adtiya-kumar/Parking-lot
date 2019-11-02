63b84d4edcc08897441856e9e8f8e76d9e1dc3ea
rishabhsingh971 <rishabh.singh@smartprix.com>
2019-11-02 21:58:42 +0530

    add status in spot

--- src/com/justpark/models/spots/Spot.java
+++ src/com/justpark/models/spots/Spot.java
@@ -2,6 +2,7 @@ package com.justpark.models.spots;
 
 public abstract class Spot {
     private final SpotType type;
+    private SpotStatus status;
 
     public SpotType getType() {
         return type;
@@ -9,6 +10,14 @@ public abstract class Spot {
 
     public Spot(SpotType type) {
         this.type = type;
+    }
+
+    public SpotStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(SpotStatus status) {
+        this.status = status;
     }
 
 }
