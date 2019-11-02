caeb2124ea9a9a800a2bc4ae0badb12476be6f81
rishabhsingh971 <rishabh.singh@smartprix.com>
2019-11-02 23:48:09 +0530

    add functions

--- src/com/justpark/models/electronics/ChargingPanel.java
+++ src/com/justpark/models/electronics/ChargingPanel.java
@@ -1,5 +1,11 @@
 package com.justpark.models.electronics;
 
-public class ChargingPanel {
+public class ChargingPanel extends Electronics {
+    void connectCharger() {
 
+    }
+
+    void disconnectCharger() {
+
+    }
 }
