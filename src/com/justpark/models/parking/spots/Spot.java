8e0698b2254551e14e430603cef0d306dcdddea3
rishabhsingh971 <rishabh.singh@smartprix.com>
2019-11-02 23:25:35 +0530

    add vehicle in spot

--- src/com/justpark/models/parking/spots/Spot.java
+++ src/com/justpark/models/parking/spots/Spot.java
@@ -1,11 +1,22 @@
 package com.justpark.models.parking.spots;
 
+import com.justpark.models.vehicles.Vehicle;
+
 public abstract class Spot {
     private final SpotType type;
     private SpotStatus status;
+    private Vehicle vehicle;
 
     public SpotType getType() {
         return type;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
     }
 
     public Spot(SpotType type) {
