88c5dd83426ae10d7273cf4c7d495e3ae932482e
rishabhsingh971 <rishabh.singh@smartprix.com>
2019-11-02 21:50:20 +0530

    Add admin actions

--- src/com/justpark/models/accounts/Admin.java
+++ src/com/justpark/models/accounts/Admin.java
@@ -1,6 +1,10 @@
 package com.justpark.models.accounts;
 
+import com.justpark.models.electronics.Electronics;
+import com.justpark.models.parking.Floor;
+import com.justpark.models.parking.Gate;
 import com.justpark.models.people.Person;
+import com.justpark.models.spots.Spot;
 
 public class Admin extends Account {
 
@@ -8,4 +12,51 @@ public class Admin extends Account {
         super(username, saltedPasswordHash, person, status);
     }
 
+    void addFloor(Floor floor) {
+
+    }
+
+    void removeFloor(Floor floor) {
+
+    }
+
+    void modifyFloor(Floor floor) {
+
+    }
+
+    void addSpot(Floor floor, Spot spot) {
+
+    }
+
+    void removeSpot(Floor floor, Spot spot) {
+
+    }
+
+    void modifySpot(Floor floor, Spot spot) {
+
+    }
+
+    void addElectronics(Floor floor, Electronics panel) {
+
+    }
+
+    void removeElectronics(Floor floor, Electronics panel) {
+
+    }
+
+    void modifyElectronics(Floor floor, Electronics panel) {
+
+    }
+
+    void addGate(Floor floor, Gate gate) {
+
+    }
+
+    void removeGate(Floor floor, Gate gate) {
+
+    }
+
+    void modifyGate(Floor floor, Gate gate) {
+
+    }
 }
