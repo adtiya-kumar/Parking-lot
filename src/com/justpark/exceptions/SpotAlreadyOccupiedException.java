6b3d498b4446ac6473e71ed46d70c25f78294885
rishabhsingh971 <rishabh.singh@smartprix.com>
2019-11-03 00:00:30 +0530

    Fix typo

---
+++ src/com/justpark/exceptions/SpotAlreadyOccupiedException.java
@@ -0,0 +1,9 @@
+package com.justpark.exceptions;
+
+public class SpotAlreadyOccupiedException extends Throwable {
+
+    public SpotAlreadyOccupiedException(String string) {
+        super(string);
+    }
+
+}
--- src/com/justpark/exceptions/SpotAlreadytOccupiedException.java
+++
@@ -1,9 +0,0 @@
-package com.justpark.exceptions;
-
-public class SpotAlreadytOccupiedException extends Throwable {
-
-    public SpotAlreadytOccupiedException(String string) {
-        super(string);
-    }
-
-}
