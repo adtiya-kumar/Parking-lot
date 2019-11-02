f295aa4c7ca09369e18e7f5bb1cd0990f9c1f63b
rishabhsingh971 <rishabh.singh@smartprix.com>
2019-11-02 21:51:51 +0530

    add attendent actions

--- src/com/justpark/models/accounts/Attendent.java
+++ src/com/justpark/models/accounts/Attendent.java
@@ -1,11 +1,24 @@
 package com.justpark.models.accounts;
 
+import com.justpark.models.parking.Ticket;
+import com.justpark.models.parking.interfaces.IssuesTicket;
+import com.justpark.models.parking.interfaces.PaymentsEnabled;
 import com.justpark.models.people.Person;
 
-public class Attendent extends Account {
+public class Attendent extends Account implements IssuesTicket, PaymentsEnabled {
 
     public Attendent(String username, String saltedPasswordHash, Person person, AccountStatus status) {
         super(username, saltedPasswordHash, person, status);
+    }
+
+    @Override
+    public Ticket giveTicket() {
+        return null;
+    }
+
+    @Override
+    public void processTicket(Ticket ticket) {
+
     }
 
 }
