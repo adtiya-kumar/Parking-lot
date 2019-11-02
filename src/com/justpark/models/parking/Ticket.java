a7967bfac98f2a7b89913fbf9fedb05f72ce10f0
rishabhsingh971 <rishabh.singh@smartprix.com>
2019-11-02 23:55:52 +0530

    remove ticket type

--- src/com/justpark/models/parking/Ticket.java
+++ src/com/justpark/models/parking/Ticket.java
@@ -3,17 +3,8 @@ package com.justpark.models.parking;
 import java.time.LocalDateTime;
 
 public class Ticket {
-    private TicketType type;
     private TicketStatus status;
     private LocalDateTime issued;
-
-    public TicketType getType() {
-        return type;
-    }
-
-    public void setType(TicketType type) {
-        this.type = type;
-    }
 
     public TicketStatus getStatus() {
         return status;
