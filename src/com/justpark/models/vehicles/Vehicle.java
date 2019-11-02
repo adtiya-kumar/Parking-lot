8cbbcefa8a4e00319dae0e60921196e0f8a0adad
rishabhsingh971 <rishabh.singh@smartprix.com>
2019-11-02 23:54:17 +0530

    Add ticket to vehicle

--- src/com/justpark/models/vehicles/Vehicle.java
+++ src/com/justpark/models/vehicles/Vehicle.java
@@ -1,14 +1,24 @@
 package com.justpark.models.vehicles;
 
 import com.justpark.models.DBObject;
+import com.justpark.models.parking.Ticket;
 
 public abstract class Vehicle extends DBObject {
     private String plateNumber;
     private String color;
     private final VehicleType type;
+    private Ticket ticket;
 
     public Vehicle(VehicleType type) {
         this.type = type;
+    }
+
+    public Ticket getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
     }
 
     public String getPlateNumber() {
