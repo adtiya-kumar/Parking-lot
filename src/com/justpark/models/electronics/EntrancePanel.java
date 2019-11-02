e969003d99edafb94416dd5e8d28f6c9bff935bc
rishabhsingh971 <rishabh.singh@smartprix.com>
2019-11-02 23:50:13 +0530

    extend display board

--- src/com/justpark/models/electronics/EntrancePanel.java
+++ src/com/justpark/models/electronics/EntrancePanel.java
@@ -1,15 +1,9 @@
 package com.justpark.models.electronics;
 
 import com.justpark.models.parking.Ticket;
-import com.justpark.models.parking.interfaces.HasDisplay;
 import com.justpark.models.parking.interfaces.IssuesTicket;
 
-public class EntrancePanel extends Electronics implements HasDisplay, IssuesTicket {
-
-    @Override
-    public void showMessage(String message) {
-
-    }
+public class EntrancePanel extends DisplayBoard implements IssuesTicket {
 
     @Override
     public Ticket giveTicket() {
