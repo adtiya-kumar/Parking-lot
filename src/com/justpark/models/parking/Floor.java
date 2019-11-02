55b46ff3aae1f0fad77e915493c45ce66b076766
rishabhsingh971 <rishabh.singh@smartprix.com>
2019-11-02 22:40:38 +0530

    add name

--- src/com/justpark/models/parking/Floor.java
+++ src/com/justpark/models/parking/Floor.java
@@ -10,9 +10,18 @@ public class Floor {
     private CustomerInfoPortal customerInfoPortal;
     private List<Spot> spots;
     private DisplayBoard displayBoard;
+    private String name;
 
     public CustomerInfoPortal getCustomerInfoPortal() {
         return customerInfoPortal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void setCustomerInfoPortal(CustomerInfoPortal customerInfoPortal) {
