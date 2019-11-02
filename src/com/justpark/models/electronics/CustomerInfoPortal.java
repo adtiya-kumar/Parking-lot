7bb1cc0db3b1dac30179f384f597425200820923
rishabhsingh971 <rishabh.singh@smartprix.com>
2019-11-02 20:45:17 +0530

    implement interface methods

--- src/com/justpark/models/electronics/CustomerInfoPortal.java
+++ src/com/justpark/models/electronics/CustomerInfoPortal.java
@@ -1,8 +1,13 @@
 package com.justpark.models.electronics;
 
-import com.justpark.models.parking.interfaces.IssuesTicket;
+import com.justpark.models.parking.Ticket;
 import com.justpark.models.parking.interfaces.PaymentsEnabled;;
 
-public class CustomerInfoPortal extends Electronics implements PaymentsEnabled, IssuesTicket {
+public class CustomerInfoPortal extends Electronics implements PaymentsEnabled {
+
+    @Override
+    public void processTicket(Ticket ticket) {
+
+    }
 
 }
